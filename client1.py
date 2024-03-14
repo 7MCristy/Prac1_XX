25ef2361607e8515499101645b3f5e754d9c59b3
Malena <115321745+7MCristy@users.noreply.github.com>
2024-03-14 17:49:45 +0100

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,5 +1,21 @@
 #lectura del fichero client.cfg
-
+'''
 with open ('client.cfg', 'r') as cl_file:
-    lines = cl_file.read().splitlines(keepends=True)
-    print(lines)
+    lines = cl_file.read().splitlines()
+    lines_treatment(lines)
+'''
+#este codigo de arriba lee por línias eol fichero y las guarda en una lista
+
+#Yo quiero un duccionario, pero probemos con la lsita
+#podemos utilizar **kwargs (key arguments)que espera un diccionario (valor, resultado) para un número indefinido de elementos
+#definiremos la función lectura de las línias
+with open ('client.cfg', 'r') as cl_file:
+    lines = cl_file.read()
+    lines_treatment(lines)
+
+
+def lines_treatment(**kwargs){
+print(kwargs)
+for key in kwargs
+    print(key,':' , kwrgs[key] )
+}
